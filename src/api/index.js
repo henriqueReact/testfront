cb6777db1fd47686e2e4fc422fbe1fab31de4383
Hinformatic <hinformatic.la@gmail.com>
2022-06-22 14:03:54 -0300

    alterando a api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'https://sheltered-escarpment-43070.herokuapp.com'
+    //baseURL: 'https://sheltered-escarpment-43070.herokuapp.com'
+    baseURL: 'https://blooming-savannah-96568.herokuapp.com'
 })
 
 export default api
